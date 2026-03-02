7154bc234153ee8f0bd827e6e6149bef34d9d6c7
Joelson Correa <joelsoncorrea.eng.compt@gmail.com>
2026-03-01 21:10:01 -0300

    arquivo main

--- main.cpp
+++ main.cpp
@@ -14,9 +14,36 @@ int main(){
     cout << "Bem-vindo ao programa de pilha!" << endl;
 
     do{
-        cout << "Digitw 0 para "
+        cout << "informe a opção desejada: " << endl;
+        cout << "Digite 0 para sair do programa." << endl;
+        cout << "Digite 1 para inserir um item na pilha." << endl;
+        cout << "Digite 2 para remover um item da pilha." << endl;
+        cout << "Digite 3 para imprimir a pilha." << endl;
+        cout << "Digite 4 para obter o tamanho atual da pilha." << endl;
+        cin >> option; // lendo a opção escolhida pelo usuário
+    } while (option != 0)
+    {
+    switch (option)
+    {    case 1:
+            cout << "Digite o item a ser inserido na pilha: ";
+            cin >> item; // lendo o item a ser inserido na pilha
+            pilha1.push(item); // chamando a função push para inserir o item na pilha
+            break;
+        case 2:
+            item = pilha1.pop();
+            break;
+
+        case 3:
+            pilha1.print(); // chamando a função print para imprimir os itens da pilha
+            break;
+
+        case 4:
+            cout << "Tamanho atual da pilha: " << pilha1.getsize() << endl; // chamando a função getsize para obter o tamanho atual da pilha
+            break;
     }
+    
 
 
+    return 0;
 
 }

bc2a5bfb1d2aee0a3bf0d7e3579869643a86a537
Joelson Correa <joelsoncorrea.eng.compt@gmail.com>
2026-03-01 21:21:04 -0300

    arquivo main switch concluido

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 using namespace std;        
 
 
-int main(){
+int main() {
 
     // criando uma instância da pilha(objeto da classe stack)
     stack pilha1;
@@ -21,29 +21,30 @@ int main(){
         cout << "Digite 3 para imprimir a pilha." << endl;
         cout << "Digite 4 para obter o tamanho atual da pilha." << endl;
         cin >> option; // lendo a opção escolhida pelo usuário
-    } while (option != 0)
-    {
     switch (option)
-    {    case 1:
+        {    case 1:
             cout << "Digite o item a ser inserido na pilha: ";
             cin >> item; // lendo o item a ser inserido na pilha
             pilha1.push(item); // chamando a função push para inserir o item na pilha
             break;
-        case 2:
-            item = pilha1.pop();
+             case 2:
+            item = pilha1.pop(); // chamando a função pop para remover um item da pilha e 
+            cout << "Item removido: " << item << endl;
             break;
 
-        case 3:
+             case 3:
             pilha1.print(); // chamando a função print para imprimir os itens da pilha
             break;
 
-        case 4:
+            case 4:
             cout << "Tamanho atual da pilha: " << pilha1.getsize() << endl; // chamando a função getsize para obter o tamanho atual da pilha
             break;
-    }
+        }
+
+    } while (option != 0); // o loop continua até que o usuário escolha a opção 0 para sair do programa
+    
     
 
-
-    return 0;
+return 0;
 
 }
